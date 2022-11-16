bad49e66c0806e01c9a1df029430cf3178160519
ShoneUnity <e10789akansh@dpsrkp.net>
2022-11-16 20:34:11 +0530

    Umm

--- app.js
+++ app.js
@@ -17,8 +17,27 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("", function (req, res) {
-  res.render("home", { fr: HomeStarters });
+app.get("/", function (req, res) {
+  res.render("home", { HomeStarters: HomeStarters });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutStarters: aboutStarters });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactStarters: contactStarters });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const blog = {
+    title: req.body.title,
+    content: req.body.someweirdstuff,
+  };
 });
 
 app.listen(port, () => {
